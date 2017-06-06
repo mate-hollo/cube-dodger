8a243fc6bba8d5a0773a449bd2c0a123f2729171
mate-hollo <hollo89@gmail.com>
2017-06-06 19:30:05 +0200

    First commit
    Added CubeDodger project files

---
+++ Tutorial_CubeDodger/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+       // rb.AddForce(0, 0, 500.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, 500.0f * Time.deltaTime);
+    }
+}
+    
