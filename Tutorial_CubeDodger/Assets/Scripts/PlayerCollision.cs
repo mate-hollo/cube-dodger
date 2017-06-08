9cb2c6fe182cd2cd0fa07ef4a48a2d17edd2815f
mate-hollo <hollo89@gmail.com>
2017-06-08 19:20:45 +0200

    commit3
    added collision and movement improvements

---
+++ Tutorial_CubeDodger/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerController playerController;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log(collision.collider.tag);
+
+        //disable movement script when hitting obstacle
+        if (collision.collider.tag == "obstacle")
+            playerController.enabled = false;
+    }
+
+}
--- Tutorial_CubeDodger/Assets/Scripts/PlayerController.cs
+++ Tutorial_CubeDodger/Assets/Scripts/PlayerController.cs
@@ -20,15 +20,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //add force to the player every frame
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-
+        
+        //add sideways force when a or d is pressed (ignore the object's mass with forcemode)
+        
         if (Input.GetKey("a"))  
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         
 
         if (Input.GetKey("d"))
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 
 
 
